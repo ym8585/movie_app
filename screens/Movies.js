872c4b927b47e1d95fc55c181aa0ffc5c35dae4e
ymlee <joo0171@naver.com>
2019-10-03 14:29:15 +0900

    #2.5

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,9 @@
 import React from "React";
 import {Text, TouchableOpacity} from "react-native";
-export default () => (
+export default ({navigation}) => (
     <>
     <Text>Movies</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+    <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
         <Text>Detail</Text>
     </TouchableOpacity>
     </>
