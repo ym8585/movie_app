299d1fc18fa9d3c5725a81a8e326cab97be78db0
ymlee <joo0171@naver.com>
2019-10-15 13:43:27 +0900

    why,,,,

---
+++ components/Section.js
@@ -0,0 +1,23 @@
+import React from "React";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.View``;
+
+const ScrollView = styled.ScrollView``;
+
+const Title = styled.Text``;
+
+const Section = ({title, movies}) => (
+    <Container>
+        <Title>{title}</Title>
+        <ScrollView>{movies.filter(movie => movie.poster_path !== null).map(movie=> null)}</ScrollView>
+    </Container>
+);
+
+Section.propTypes = {
+    movies: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired
+};
+
+export default Section;
